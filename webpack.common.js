8d95aeb6189c1aa21c4b2d4c445ca5f77621f79d
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-06-11 14:00:41 -0600

    chore: min changes

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./src/main.js",
+    app: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
