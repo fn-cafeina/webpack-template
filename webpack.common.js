20f1fd25fe32124394aca0fd6ecf68aac2912c60
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-02-09 16:52:20 -0600

    Add avif to webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -24,7 +24,7 @@ export default {
         use: ["html-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|avif|gif)$/i,
         type: "asset/resource",
       },
       {
